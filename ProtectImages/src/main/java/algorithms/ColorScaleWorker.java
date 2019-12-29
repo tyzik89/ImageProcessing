d8ccbc0e0b1c7781165057982f5040bf7e1b5a6a
Vladimir <work.vladimir.s@gmail.com>
2019-12-29 22:59:32 +0300

    Рефакториг

---
+++ ProtectImages/src/main/java/algorithms/ColorScaleWorker.java
@@ -0,0 +1,21 @@
+package algorithms;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class ColorScaleWorker {
+
+    public static Mat doGrayscale(Mat rgbSegment) {
+        //Конвертируем изображение в одноканальное
+        Mat matGray = new Mat();
+        Imgproc.cvtColor(rgbSegment, matGray, Imgproc.COLOR_BGR2GRAY);
+        return matGray;
+    }
+
+    public static Mat doBinary (Mat graySegment) {
+        //Перевод в бинарное изображение
+        Mat matBinary = new Mat();
+        Imgproc.threshold(graySegment, matBinary, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU);
+        return matBinary;
+    }
+}
--- ProtectImages/src/main/java/app/MainApp.java
+++ ProtectImages/src/main/java/app/MainApp.java
@@ -6,6 +6,7 @@ import javafx.stage.Stage;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import utils.ImageUtils;
+import utils.ShowImage;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -19,29 +20,6 @@ public class MainApp extends Application {
         Image image = new Image(localUrl);
 
         Mat sourceMat = ImageUtils.imageFXToMat(image);
-
-        Algorithm algorithm = new Algorithm();
-
-        //Разбиваем изображение на сегменты
-        ArrayList<Mat> segments = algorithm.doSegmentation(sourceMat, 8);
-        ArrayList<Mat> segmentsWithHash = new ArrayList<>();
-
-        for (Mat originalSegment : segments) {
-            //Каждый сегмент переводим в градацию серого
-            Mat segmentGray = algorithm.doGrayscale(originalSegment);
-            //Бинаризируем сегмет
-            Mat segmentBinary = algorithm.doBinary(segmentGray);
-            //Получаем хэш-код каждого сегмента и встраиваем этот хэш код в сегмент
-            //ShowImage.show(ImageUtils.matToImageFX(originalSegment));
-            algorithm.doSteganography(segmentBinary, originalSegment);
-            //ShowImage.show(ImageUtils.matToImageFX(originalSegment));
-           // break;
-        }
-
-        //Собираем изображение из сегментов со встроеным хэш-кодом
-        algorithm.doSynthesis(segments, sourceMat);
-        image = ImageUtils.matToImageFX(sourceMat);
-        //ShowImage.show(image);
     }
 
     public static void main(String[] args) throws Exception {
