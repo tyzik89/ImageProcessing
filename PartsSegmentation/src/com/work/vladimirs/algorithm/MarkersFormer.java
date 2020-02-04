c62e0bd967ef3d433fc76656213a0d1c40c62b3e
Vladimir <work.vladimir.s@gmail.com>
2020-02-04 16:26:04 +0300

    Fix find markers

--- PartsSegmentation/src/com/work/vladimirs/algorithm/MarkersFormer.java
+++ PartsSegmentation/src/com/work/vladimirs/algorithm/MarkersFormer.java
@@ -2,6 +2,7 @@ package com.work.vladimirs.algorithm;
 
 import com.work.vladimirs.algorithm.entities.Marker;
 import com.work.vladimirs.utils.ImageUtils;
+import com.work.vladimirs.utils.ShowImage;
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 
@@ -10,7 +11,7 @@ import java.util.Arrays;
 
 public class MarkersFormer {
 
-    private static final double distanceOfMarkers = 0.3;
+    private static final double distanceOfMarkers = 1.0;
     private static final double ratioLength = 0.2;
     private Mat vectorOfLines;
     private Mat sourceMat;
@@ -33,6 +34,7 @@ public class MarkersFormer {
                 double[] line = vectorOfLines.get(i, j);
                 startPointOfLine = new Point(line[0], line[1]);
                 endPointOfLine = new Point(line[2], line[3]);
+                //System.out.println(startPointOfLine.x + " " + startPointOfLine.y);
 
                 Marker firstMarker = new Marker();
                 Marker secondMarker = new Marker();
@@ -42,11 +44,31 @@ public class MarkersFormer {
                 //Уменьшаем маркер, чтобы он был чуть меньше границы объекта
                 reduceMarkerLength(firstMarker, ratioLength);
                 reduceMarkerLength(secondMarker, ratioLength);
+                //System.out.println(firstMarker.getStartPoint().x + " " + firstMarker.getStartPoint().y);
 
+                //Определяем тип маркера, сравнивая фон оригинального изображения
+                MarkersGradientComparator comparator = new MarkersGradientComparator(sourceMat);
+                comparator.compare(firstMarker, secondMarker);
 
+                createMaskWithMarker(firstMarker);
             }
+            break;
         }
         return maskOfMarkers;
+    }
+
+    private Mat createMaskWithMarker(Marker m) {
+        //Маска с маркером
+        Mat maskWithMarker = new Mat(sourceMat.size(), CvType.CV_8UC1, ImageUtils.COLOR_BLACK);
+        Imgproc.line(
+                maskWithMarker,
+                m.getStartPoint(),
+                m.getEndPoint(),
+                ImageUtils.COLOR_WHITE,
+                1,
+                4);
+        ShowImage.show(ImageUtils.matToImageFX(maskWithMarker), "Mask with marker");
+        return maskWithMarker;
     }
 
     private void reduceMarkerLength(Marker m, double ratio) {
--- PartsSegmentation/src/com/work/vladimirs/algorithm/MarkersGradientComparator.java
+++ PartsSegmentation/src/com/work/vladimirs/algorithm/MarkersGradientComparator.java
@@ -2,11 +2,14 @@ package com.work.vladimirs.algorithm;
 
 import com.work.vladimirs.algorithm.entities.Marker;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 public class MarkersGradientComparator implements Comparator<Marker> {
 
+    private static final double EPSILON = 0.0001;
     private Mat originalMat;
 
     public MarkersGradientComparator(Mat originalMat) {
@@ -15,9 +18,103 @@ public class MarkersGradientComparator implements Comparator<Marker> {
 
     @Override
     public int compare(Marker m1, Marker m2) {
-        double[] buffer = new double[originalMat.channels() * originalMat.rows() * originalMat.cols()];
-        originalMat.get(0, 0, buffer);
+        ArrayList<Point> pointListOfMarkerM1 = line(m1.getStartPoint().x, m1.getStartPoint().y, m1.getEndPoint().x, m1.getEndPoint().y);
+        ArrayList<Point> pointListOfMarkerM2 = line(m2.getStartPoint().x, m2.getStartPoint().y, m2.getEndPoint().x, m2.getEndPoint().y);
+        double gradientValueM1 = findMarkerGradient(pointListOfMarkerM1);
+        double gradientValueM2 = findMarkerGradient(pointListOfMarkerM2);
+        System.out.println("gradientValueM1: " + gradientValueM1 + ", gradientValueM2: " + gradientValueM2);
+        return Double.compare(gradientValueM1, gradientValueM2);
+    }
 
-        return 0;
+    private double findMarkerGradient(ArrayList<Point> pointListOfMarker) {
+        double value = 0;
+        for (Point point : pointListOfMarker) {
+            double[] pixelChannels = originalMat.get((int) point.x, (int) point.y);
+            double brightnessPixel = (pixelChannels[0] + pixelChannels[1] + pixelChannels[2]) / 3;
+            value += brightnessPixel;
+        }
+        return value / pointListOfMarker.size();
+    }
+
+    private boolean isEqual(double x, double y){
+        return Math.abs(x-y) < EPSILON;
+    }
+
+
+    /**
+     * реализация алгоритма Брезенхема
+     * @param x
+     * @param y
+     * @param x2
+     * @param y2
+     * @return
+     */
+    private ArrayList<Point> line(double x, double y, double x2, double y2) {
+        ArrayList<Point> pointArray = new ArrayList<>();
+        double w = x2 - x ;
+        double h = y2 - y ;
+        double dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0 ;
+        if (w<0) dx1 = -1 ; else if (w>0) dx1 = 1 ;
+        if (h<0) dy1 = -1 ; else if (h>0) dy1 = 1 ;
+        if (w<0) dx2 = -1 ; else if (w>0) dx2 = 1 ;
+        int longest = (int) Math.abs(w) ;
+        int shortest = (int) Math.abs(h) ;
+        if (!(longest>shortest)) {
+            longest = (int) Math.abs(h) ;
+            shortest = (int) Math.abs(w) ;
+            if (h<0) dy2 = -1 ; else if (h>0) dy2 = 1 ;
+            dx2 = 0 ;
+        }
+        int numerator = longest >> 1 ;
+        for (int i=0;i<=longest;i++) {
+            pointArray.add(new Point(x, y));
+            numerator += shortest ;
+            if (!(numerator<longest)) {
+                numerator -= longest ;
+                x += dx1 ;
+                y += dy1 ;
+            } else {
+                x += dx2 ;
+                y += dy2 ;
+            }
+        }
+
+        return pointArray;
     }
 }
+
+
+
+
+
+/*
+    public void line(int x,int y,int x2, int y2, int color) {
+        int w = x2 - x ;
+        int h = y2 - y ;
+        int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0 ;
+        if (w<0) dx1 = -1 ; else if (w>0) dx1 = 1 ;
+        if (h<0) dy1 = -1 ; else if (h>0) dy1 = 1 ;
+        if (w<0) dx2 = -1 ; else if (w>0) dx2 = 1 ;
+        int longest = Math.abs(w) ;
+        int shortest = Math.abs(h) ;
+        if (!(longest>shortest)) {
+            longest = Math.abs(h) ;
+            shortest = Math.abs(w) ;
+            if (h<0) dy2 = -1 ; else if (h>0) dy2 = 1 ;
+            dx2 = 0 ;
+        }
+        int numerator = longest >> 1 ;
+        for (int i=0;i<=longest;i++) {
+            putpixel(x,y,color) ;
+            numerator += shortest ;
+            if (!(numerator<longest)) {
+                numerator -= longest ;
+                x += dx1 ;
+                y += dy1 ;
+            } else {
+                x += dx2 ;
+                y += dy2 ;
+            }
+        }
+    }
+*/
