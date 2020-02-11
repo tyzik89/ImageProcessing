eadfd8776dee76aa4c7c85cb258b15cc721068d1
Vladimir <work.vladimir.s@gmail.com>
2020-02-11 12:53:18 +0300

    Работа с массивом линий

--- PartsSegmentation/src/com/work/vladimirs/algorithm/MarkersFormer.java
+++ PartsSegmentation/src/com/work/vladimirs/algorithm/MarkersFormer.java
@@ -23,8 +23,39 @@ public class MarkersFormer {
     public Mat prepareMaskOfMarkers() {
         // Создание маркерного изображения для алгоритма водоразделов
         Mat maskWithMarker = new Mat(sourceMat.size(), CvType.CV_32S, ImageUtils.COLOR_BLACK);
-        Line currentLine;
+        ArrayList<Line> lines = getArrayOfLines(vectorOfLines);
 
+        for (Line currentLine : lines) {
+            //Проверяем линию на "надёжность" по длине
+            if (!LineValidator.validateLineLength(currentLine)) continue;
+
+            Line firstMarker = new Line();
+            Line secondMarker = new Line();
+            //Находим параллельные маркеры для этой линии, лежащие на определенном растоянии от линии
+            findParallelMarkers(currentLine, firstMarker, secondMarker, distanceOfMarkers);
+
+            //Уменьшаем маркер, чтобы он был чуть меньше границы объекта
+            reduceMarkerLength(firstMarker, ratioLength);
+            reduceMarkerLength(secondMarker, ratioLength);
+
+            //Определяем тип маркера, сравнивая фон оригинального изображения
+            //Фон ТЕМНЕЕ, это значит что это маркер фона.
+            MarkersGradientComparator gradientComparator = new MarkersGradientComparator(sourceMat);
+            int comp = gradientComparator.compare(firstMarker, secondMarker);
+            if (comp < 0) {
+                createMaskWithMarker(firstMarker, maskWithMarker, ImageUtils.COLOR_GRAY);
+                createMaskWithMarker(secondMarker, maskWithMarker, ImageUtils.COLOR_WHITE);
+            } else if (comp > 0) {
+                createMaskWithMarker(firstMarker, maskWithMarker, ImageUtils.COLOR_WHITE);
+                createMaskWithMarker(secondMarker, maskWithMarker, ImageUtils.COLOR_GRAY);
+            }
+        }
+        return maskWithMarker;
+    }
+
+    private ArrayList<Line> getArrayOfLines(Mat vectorOfLines) {
+        ArrayList<Line> lineArrayList = new ArrayList<>();
+        Line currentLine;
         //Бежим по всем найденным линиям
         for (int i = 0, r = vectorOfLines.rows(); i < r; i++) {
             for (int j = 0, c = vectorOfLines.cols(); j < c; j++) {
@@ -32,45 +63,10 @@ public class MarkersFormer {
                 double[] line = vectorOfLines.get(i, j);
                 //todo ВНИМАНИЕ! ИНВЕРТИРУЕМ ОСИ КООРДИНАТ!
                 currentLine = new Line(new Point(line[1], line[0]), new Point(line[3], line[2]));
-
-                //Проверяем линию на "надёжность" по длине
-                if (!LineValidator.validateLineLength(currentLine)) continue;
-
-                Line firstMarker = new Line();
-                Line secondMarker = new Line();
-                //Находим параллельные маркеры для этой линии, лежащие на определенном растоянии от линии
-                findParallelMarkers(currentLine, firstMarker, secondMarker, distanceOfMarkers);
-
-//                System.out.println("Исходная линия: " + "start:{" + currentLine.getStartPoint().x + "," + currentLine.getStartPoint().y + "}," + " end:{" + currentLine.getEndPoint().x + "," + currentLine.getEndPoint().y + "}");
-//                System.out.println("Маркер_1: " + "start:{" + firstMarker.getStartPoint().x + "," + firstMarker.getStartPoint().y + "}," + " end:{" + firstMarker.getEndPoint().x + "," + firstMarker.getEndPoint().y + "}");
-//                System.out.println("Маркер_2: " + "start:{" + secondMarker.getStartPoint().x + "," + secondMarker.getStartPoint().y + "}," + " end:{" + secondMarker.getEndPoint().x + "," + secondMarker.getEndPoint().y + "}");
-
-                //Уменьшаем маркер, чтобы он был чуть меньше границы объекта
-                reduceMarkerLength(firstMarker, ratioLength);
-                reduceMarkerLength(secondMarker, ratioLength);
-
-//                System.out.println("Маркер_1_reduce: " + "start:{" + firstMarker.getStartPoint().x + "," + firstMarker.getStartPoint().y + "}," + " end:{" + firstMarker.getEndPoint().x + "," + firstMarker.getEndPoint().y + "}");
-//                System.out.println("Маркер_2_reduce: " + "start:{" + secondMarker.getStartPoint().x + "," + secondMarker.getStartPoint().y + "}," + " end:{" + secondMarker.getEndPoint().x + "," + secondMarker.getEndPoint().y + "}");
-
-//                ShowImage.drawPointsBetweenTwoPoints(sourceMat, currentLine.getStartPoint().x, currentLine.getStartPoint().y, currentLine.getEndPoint().x, currentLine.getEndPoint().y, new double[]{255, 0, 0});
-//                ShowImage.drawPointsBetweenTwoPoints(sourceMat, firstMarker.getStartPoint().x, firstMarker.getStartPoint().y, firstMarker.getEndPoint().x, firstMarker.getEndPoint().y, new double[]{0, 0, 255});
-//                ShowImage.drawPointsBetweenTwoPoints(sourceMat, secondMarker.getStartPoint().x, secondMarker.getStartPoint().y, secondMarker.getEndPoint().x, secondMarker.getEndPoint().y, new double[]{0, 0, 255});
-//                ShowImage.show(ImageUtils.matToImageFX(sourceMat), "LINES");
-
-                //Определяем тип маркера, сравнивая фон оригинального изображения
-                //Фон ТЕМНЕЕ, это значит что это маркер фона.
-                MarkersGradientComparator gradientComparator = new MarkersGradientComparator(sourceMat);
-                int comp = gradientComparator.compare(firstMarker, secondMarker);
-                if (comp < 0) {
-                    createMaskWithMarker(firstMarker, maskWithMarker, ImageUtils.COLOR_GRAY);
-                    createMaskWithMarker(secondMarker, maskWithMarker, ImageUtils.COLOR_WHITE);
-                } else if (comp > 0) {
-                    createMaskWithMarker(firstMarker, maskWithMarker, ImageUtils.COLOR_WHITE);
-                    createMaskWithMarker(secondMarker, maskWithMarker, ImageUtils.COLOR_GRAY);
-                }
+                lineArrayList.add(currentLine);
             }
         }
-        return maskWithMarker;
+        return lineArrayList;
     }
 
     private void createMaskWithMarker(Line m, Mat maskWithMarker, Scalar color) {
@@ -88,22 +84,6 @@ public class MarkersFormer {
                 color,
                 1,
                 4);
-
-/*        // Находим контуры маркеров
-        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Imgproc.findContours(imageWithMarker, contours, new Mat(),
-                Imgproc.RETR_CCOMP,
-                Imgproc.CHAIN_APPROX_SIMPLE);
-
-        // Отрисовываем контуры нужным цветом
-        for (int i = 0; i < contours.size(); i++) {
-            Imgproc.drawContours(maskWithMarker, contours, i, color, 1);
-        }*/
-
-         //Отрисовка отдельного маркера
-//        Mat markers = new Mat();
-//        maskWithMarker.convertTo(markers, CvType.CV_8U);
-//        ShowImage.show(ImageUtils.matToImageFX(markers), "Mask with marker");
     }
 
     private void reduceMarkerLength(Line m, double ratio) {
